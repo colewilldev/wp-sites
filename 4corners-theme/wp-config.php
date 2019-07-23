7a43f6fae976ba8d5d942b5f3a2af1035f432093
Cole Williams <wcole2222@gmail.com>
2019-07-22 21:40:39 -0600

    more work on clean biz

--- 4corners-theme/wp-config.php
+++ 4corners-theme/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '4corners-theme' );
+define( 'DB_NAME', 'colewi7_4corners' );
 
 /** MySQL database username */
 define( 'DB_USER', 'cdubs' );
